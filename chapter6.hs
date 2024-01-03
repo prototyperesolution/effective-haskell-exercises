615c02fac23731c8bc3c756c85dfc807e72798b2
Joe <86804949+prototyperesolution@users.noreply.github.com>
2024-01-03 13:38:18 +0000

    chapter 6

--- chapter6.hs
+++ chapter6.hs
@@ -1,3 +1,5 @@
+import Prelude hiding (null)
+
 newtype Sum = Sum { getSum :: Int } deriving Show
 
 instance Semigroup Sum where
@@ -5,4 +7,25 @@ instance Semigroup Sum where
 
 instance Monoid Sum where
     mempty = Sum 0
+
+--writing type classes representing emptiness
+
+class Eq a => Nullable a where
+    isNull :: a -> Bool
+    isNull a = a == null
+    null :: a
+
+instance Nullable a => Nullable (Maybe a) where
+    isNull Nothing = True
+    isNull _ = False
+    null = Nothing
+
+instance (Nullable a, Nullable b) => Nullable (a,b) where
+    isNull (a, b) = isNull a && isNull b
+    null = (null, null)
+
+instance Eq a => Nullable [a] where
+    isNull [] = True
+    isNull _ = False
+    null = []
 
