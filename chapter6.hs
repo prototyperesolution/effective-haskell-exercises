f1382ce2fa1674e7c9fc01b99887156aa6fd132a
Joe <86804949+prototyperesolution@users.noreply.github.com>
2024-01-10 18:11:16 +0000

    redoing chapter 6

--- chapter6.hs
+++ chapter6.hs
@@ -8,6 +8,26 @@ instance Semigroup Sum where
 instance Monoid Sum where
     mempty = Sum 0
 
+--creating record structure type class
+data Natural a = Natural {
+     equal :: a -> a -> Bool
+    , add :: a -> a -> a
+    , multiply :: a -> a -> a
+    , additiveIdentity :: a
+    , multiplicativeIdentity :: a
+    , displayAsString :: a -> String
+}
+
+intNatural :: Natural Int
+intNatural = Natural { 
+    equal = (==)
+    , add = (+)
+    , multiply = (*)
+    , additiveIdentity = 0
+    , multiplicativeIdentity = 1
+    , displayAsString = show
+    }
+
 --writing type classes representing emptiness
 
 class Eq a => Nullable a where
